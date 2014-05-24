0d90af794c9c4181aab92f1231440e4219cc18ac
liquidx <liquidx@163.com>
2014-05-24 17:03:14 +0800

    up

---
+++ assets/js/main_cn.js
@@ -0,0 +1,27 @@
+require.config({
+    baseUrl: '../assets/js',
+    paths: {
+        jquery: 'lib/jquery',
+        underscore: 'lib/underscore',
+        backbone: 'lib/backbone'
+    },
+    shim: {
+        'backbone': {
+            //These script dependencies should be loaded before loading
+            //backbone.js
+            deps: ['underscore', 'jquery'],
+            //Once loaded, use the global 'Backbone' as the
+            //module value.
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        },
+    }
+});
+
+require(["jquery", "app/app"], function($, stargazers) {
+    $(document).ready(function() {
+		stargazers.init();
+    });
+});
