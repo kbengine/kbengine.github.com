27e0b5ead7a7f44c0d6c52f4fd22aa2ddb381ec3
liquidx <liquidx@163.com>
2014-05-26 10:43:37 +0800

    up

--- assets/js/app/models/Repo.js
+++ assets/js/app/models/Repo.js
@@ -6,15 +6,22 @@ define(['jquery', 'underscore', 'backbone', 'app/collections/Stargazers'], funct
 			var stargazers = new Stargazers();
 			stargazers.url = this.get('stargazers_url');
 
+			stargazers.comparator = function(stargazer) {
+				return Math.random()>0.5?(-1):1;
+			};
+				
 			var that = this;
 			stargazers.fetch().complete(function() {
 				stargazers.sort(function(a,b){return Math.random()>0.5?(-1):1});
 				that.set('stargazers', stargazers);
 			});
 			
-			
 			var forks = new Stargazers();
 			forks.url = this.get('forks_url');
+			
+			forks.comparator = function(fork) {
+				return Math.random()>0.5?(-1):1;
+			};
 			
 			forks.fetch().complete(function() {
 				forks.sort(function(a,b){return Math.random()>0.5?(-1):1});
